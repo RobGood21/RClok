4794ef7c5319090370fe453d166b39f439441ca5
Rob Antonisse <robantonisse@gmail.com>
2021-03-22 17:54:38 +0100

    17.54

--- lokomotief/lokomotief.ino
+++ lokomotief/lokomotief.ino
@@ -1,7 +1,10 @@
 /*
  Name:		Sketch1.ino
  Created:	3/22/2021 5:52:00 PM
- Author:	gebruiker
+ Author:	Rob Antonisse
+
+ Dit is de laatste aanpassing
+
 */
 
 // the setup function runs once when you press reset or power the board
