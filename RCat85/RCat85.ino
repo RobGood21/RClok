a2ef61e241227f608affff4db0a03547399210b6
Rob Antonisse <robantonisse@gmail.com>
2021-03-31 11:59:55 +0200

    Motor sturing van uit processing 1e fase werkt alleen freq en duty

--- RCat85/RCat85.ino
+++ RCat85/RCat85.ino
@@ -3,104 +3,49 @@
  Created:	3/25/2021 8:55:18 PM
  Author:	Rob Antonisse
 
- Diverse testen met attiny85, uiteindelijk doel een RC receiver maken met attiny en NRF24
+Sketch voor testen met een NRF24L01 mini smd aangestuurd door een Attiny85
+Attiny op 8Mhz internal.
+Zie document Attiny85; Arduino as ISP (drive) voor meer info.
+Alle onnodig regels zijn hier verwijderd.
 
  */
-
-
- //#include "SPI.h"
- //#include "RF24.h"
-
 #include <SPI.h>
-//#include <RF24_config.h>
 #include <RF24.h>
-//#include <printf.h>
-//#include <nRF24L01.h>
 
-RF24 radio(10,4); //nieuwe pinnen, CE CSN, constructor class RF24 
-//byte address[][6] = { "TXDCC","RD000" }; //is adres is van de TX 2e wordt niet gebruikt nodig als er moet worden geantwoord
+RF24 radio(10, 4); //CE CSN, constructor class RF24 (pin 10 bestaat niet, Ce pin NRF24 altijd aan VCC) CSN=SlaveSelect van NRF24
+//byte address[][6] = { "TXDCC","RD000" }; //methode als ook een retour adres nodig is. bi-directioneel.
+byte testadres[6] = { "TXDCC" }; //Adres array van 6 bytes, bij TX en RX hetzelfde. (letters worden omgezet in ascii)
+byte RX_data[4]; //Array voor 4 bytes te ontvangen data
+byte SFT_data = 0x0; //byte wat met shiftregister 8 extra pinnen geeft
 
-byte testadres[6] = { "TXDCC" }; //dit werkt ook
-
-byte RX_data[4]; //array pointer ernaar wordt telkens gebruikt dus &RX_data
-byte SFT_data=0x0;
-unsigned long ledtimer;
-
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-DDRB |= (1 << 3);
-PORTB &= ~(1 << 3);
-
-	radio.begin();
-	radio.setChannel(50);
-	radio.setPALevel(RF24_PA_LOW);
-	radio.setPayloadSize(4); //max te ontvangen bytes
-	//radio.openReadingPipe(1, address[0]); // using pipe 1, adress 0 (TX adres hoop ik....)	
-	radio.openReadingPipe(1, testadres);
-
-	radio.startListening(); // put radio in RX mode
-	//radio.setAutoAck(true);
-	SFT_spi();
+	//*******NRF24L01
+	radio.begin(); //open class RF27 
+	radio.setChannel(50);//instellen freq, kanaal. (1~125 opties)
+	radio.setPALevel(RF24_PA_LOW);//power level(en dus range) van de Transmitter
+	radio.setPayloadSize(4); //max te ontvangen bytes. Anders altijd 32 bytes. Dit versnelt de boel.
+	radio.openReadingPipe(1, testadres); //open RX kenmerk pipe 1 adres testadres, alleen tx met dit adres worden verwerkt.
+	radio.startListening(); //start proces
+	//radio.setAutoAck(true); //staat altijd aan, standaard
+	//*****SN74HC595
+	DDRB |= (1 << 3); //ss van shiftregister, RCLK
+	SFT_spi(); //shift SPI, functie start, clears shiftregister pins 0~7
 }
-
-// the loop function runs over and over again until power down or reset
-
-
 void loop() {
-
-	uint8_t pipe;
-
-if (radio.available(&pipe)) {// is there a payload? get the pipe number that recieved it
-			radio.read(&RX_data, 4);
-			//PINB |= (1 << 3);
-			
-			SFT_data = 0;
-			for (byte i = 0; i < 4; i++) {
-
-				if (RX_data[i] > 0)SFT_data |= (1 << i);
-			}
-			SFT_spi();
-			//SFT_exe();
+	byte pipeVanDeZender;
+	if (radio.available(&pipeVanDeZender)) {// is data ontvanegn? Van welke zender (pipe max 6 mogelijk)
+		radio.read(&RX_data, 4); //plaats de ontvangen data in array RX_data (4 bytes)
+		SFT_data = 0; //clear en vul het uit te shiften byte
+		for (byte i = 0; i < 4; i++) { 
+			if (RX_data[i] > 0)SFT_data |= (1 << i);
 		}
-
-	//if (millis() - ledtimer > 100) {
-	//	ledtimer = millis();
-	//		}
+		SFT_spi();
+	}
 }
 void SFT_spi() {
-
-	//radio.stopListening();
-	//SPI.begin();
-	//SPI.beginTransaction(SPISettings(9600, MSBFIRST, SPI_MODE0));
-	SPI.transfer(SFT_data);
-	PORTB |= (1 << 3);
-	//delay(5);
+	SPI.transfer(SFT_data); //maakt gebruik van de SPI library
+	PORTB |= (1 << 3); //latch SFT_data in het shiftregister
 	PORTB &= ~(1 << 3);
-	//SPI.endTransaction();	
 }
 
-void SFT_exe() {/*
-	Rclk=PB3
-	SRCLK=PB2
-	SERin=PB1	
-	CSN van NRF=PB4
-	*/
-	PORTB &= ~(1 << 4); //csn NRF24 (chip select NOT)
-	PORTB &= ~(1 << 3); 
-	PORTB &= ~(1 << 2);
 
-	for (byte i = 0; i < 8; i++) {
-		PORTB &= ~(1 << 1);	
-		if (SFT_data & (1 << i))PORTB |= (1 << 1);
-		PINB |= (1 << 2); PINB |= (1 << 2);
-	}
-	PINB |= (1 << 3); 
-	delay(20);
-	PINB |= (1 << 3);
-
-	if (SFT_data > 0)PORTB |= (1 << 3);
-
-	//PORTB &= ~(1 << 4); //csn NRF24 (chip select NOT)
-	//shift out
-}
--- lokomotief/lokomotief.ino
+++ lokomotief/lokomotief.ino
@@ -1,18 +1,158 @@
 /*
- Name:		Sketch1.ino
+ Name:		Lokomotief.ino
  Created:	3/22/2021 5:52:00 PM
  Author:	Rob Antonisse
 
- Dit is de laatste aanpassing, gedaan
+Diverse ontwikkeling sketch Lokomotief,
+Besturing via RC control, power uit lion accu
+
+29mrt2021
+Shiftregister instellen via SPI
 
 */
 
-// the setup function runs once when you press reset or power the board
+#include <SPI.h>
+
+//Timer1 gebruiken (attiny heeft die ook, alleen afwijkende prescaler)
+
+//declaraties
+byte Shift_byte = 0;
+//byte drive; //0=stop 1=Forward 2=reverse
+byte fase;
+byte dir;
+
+unsigned long tt;
+byte extraPrescaler;
+
+byte inPar;
+byte inVal;
+byte inByte;
+byte countRX;
+
+byte duty = 250; //hoe breed is de puls
+byte freq = 250;
+; //hoe vaak komt de puls, startmoment
+
+//byte parameter;
+//byte waarde;
+
 void setup() {
+	Serial.begin(9600);
+
+	SER_write(0xFF, 0x00); //send wakeup call
+
+	SPI.begin();
+
+	DDRB |= (1 << 1);// Pin9 as output SS van HC595
+	PORTB &= ~(1 << 1); //clear Pin9
+	shift(); //clear outputs shiftregister
+
+	//Timer2 instellen,	8 bits timer as in Attiny
+	TCCR2A = 0;
+	TCCR2A |= (1 << 1);//CTC mode set WGM21
+
+	TCCR2B |= (3 << 0); //prescaler(max)
+
+	OCR2A = 5; //tijdsduur 0-OCR2A
+
+	TIMSK2 |= (1 << 1);//enable timer2_compa interrupt	
 
 }
 
-// the loop function runs over and over again until power down or reset
+ISR(TIMER2_COMPA_vect) {
+	//TCNT2 = 0x01;
+	extraPrescaler++;
+	if (extraPrescaler > 50) {
+		
+		extraPrescaler = 0;
+		//Shift_byte ^= (1 << 1);
+
+		switch (fase) {
+		case 0:
+			Shift_byte &= ~(1 << 1);
+			OCR2A = freq; //pauze tussen pulsen
+			//Serial.print("*");
+			fase = 1;
+			break;
+		case 1:
+			Shift_byte |= (1 << 1);
+			OCR2A = duty; //duur puls
+			//Serial.print("-");
+			fase = 0;
+			break;
+		}
+		shift();
+	}
+}
+
+
 void loop() {
-  
+	SER_read();
+	if (millis() - tt > 1000) {
+		tt = millis();
+		//parameter++;
+		//waarde = waarde + 5;
+		//SER_rx();
+		//SER_write(parameter, waarde);
+
+	}
+
+}
+
+void shift() {
+	SPI.transfer(Shift_byte);
+	PINB |= (1 << 1); PINB |= (1 << 1); //make RCLK puls, latch
+}
+
+void SER_write(byte para, byte value) {
+	Serial.write(0xFF); //Serial.write(state);
+	Serial.write(para);
+	Serial.write(value);
+}
+void SER_read() {
+	while (Serial.available() > 0) {
+		inByte = Serial.read();
+		if (inByte == 0xFF) {
+			countRX = 1;
+
+		}
+		else {
+			switch (countRX) {
+			case 0: //doe niks start ontvangst 
+				break;
+			case 1: //ontvang parameter max 254
+				inPar = inByte;
+				countRX++;
+				break;
+			case 2: //ontvang value max 254
+				inVal = inByte;
+				countRX = 0;
+				SER_rx();
+				break;
+			}
+		}
+	}
+
+}
+void SER_rx() {
+	//verwerkt ontvangen  boodschap
+	//SH_data = random(0, 255);//inVal;
+	//shift();
+	switch (inPar) {
+	case 10: //Motor duty, breedte van de puls
+		freq = inVal;
+		break;
+	case 20: //Motor freq, tijd tussen puls uit en puls aan. 
+		duty = inVal;
+
+		break;
+	case 30: //Motor richting 0=Stop 1=forward 2= reversed ? of anders
+
+		break;
+	}
+
+	SER_write(inPar, inVal); //terug bouncen naar Processing	
+
+		//Serial.print("ontvangen: parameter "); Serial.print(inPar); Serial.print(" Value: "); Serial.println(inVal);
+
 }
